632786a75614bbf43155fd026bf6614bfdda75a6
Callum <Callum.B161@btinternet.com>
2019-06-25 18:54:49 +0100

    C1

--- src/computerSystem/forms/Master.java
+++ src/computerSystem/forms/Master.java
@@ -15,9 +15,11 @@ public class Master extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("Master.fxml"));
         Parent root = FXMLLoader.load(getClass().getResource("Master.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.setMinHeight(400); primaryStage.setMinWidth(600);
         primaryStage.show();
     }
 
