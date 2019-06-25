197540fc78bb2b730e69f54af277b446bd6d8b47
Callum <Callum.B161@btinternet.com>
2019-06-25 21:40:46 +0100

    FXML loader has been created, the container is becoming functional. Work still required to get each button working & we could do with improved icons

--- src/computerSystem/forms/Master.java
+++ src/computerSystem/forms/Master.java
@@ -1,10 +1,23 @@
 package computerSystem.forms;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import org.w3c.dom.events.MouseEvent;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class Master extends Application {
 
@@ -13,15 +26,47 @@ public class Master extends Application {
         launch();
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("Master.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("Master.fxml"));
-        primaryStage.setTitle("Hello World");
+    private Parent root;
+    @FXML private AnchorPane MasterContainer;
+
+    @Override public void start(Stage primaryStage) throws Exception{
+        //Initialize the JavaFX Application
+        root = FXMLLoader.load(getClass().getResource("Master.fxml"));
+        primaryStage.setTitle("3CS Computers");
         primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.setMinHeight(400); primaryStage.setMinWidth(600);
         primaryStage.show();
+
+        //Load the default "Home" page onto the above
+        loadPage("Home.fxml");
     }
+
+    //Container Methods
+    private void loadPage(String FXMLFile) throws IOException {
+        AnchorPane pageContainer = (AnchorPane) root.lookup("#loaderAnchorPane");
+        pageContainer.getChildren().removeAll();
+        AnchorPane LoadingPane = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        //LoadingPane.setPrefWidth(pageContainer.getWidth());
+        //LoadingPane.setPrefHeight(pageContainer.getHeight());
+        AnchorPane.setTopAnchor(LoadingPane, 0.0);
+        AnchorPane.setBottomAnchor(LoadingPane, 0.0);
+        AnchorPane.setLeftAnchor(LoadingPane, 0.0);
+        AnchorPane.setRightAnchor(LoadingPane, 0.0);
+        pageContainer.getChildren().add((LoadingPane));
+    }
+
+    //Events
+    @FXML private void handleClick(Event ev) {
+        System.out.println("clicc");
+        HBox box = (HBox)ev.getSource();
+        for (Node ctrl: box.getChildren()) {
+            if(ctrl instanceof Label)
+            {
+                ((Label) ctrl).setText("Clicked");
+            }
+        }
+    }
+
 
 
 }
